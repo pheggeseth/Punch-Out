3e25692383c215d52fe2026b3902cc9d5b87dcdb
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-23 00:55:33 -0500

    Started projects view layout

--- server/public/scripts/controllers/projects.controller.js
+++ server/public/scripts/controllers/projects.controller.js
@@ -1,4 +1,26 @@
 app.controller('ProjectsController', ['$http', function($http) {
   const vm = this;
-  vm.message = 'Hello from ProjectsController';
+  // vm.message = 'Hello from ProjectsController';
+
+  vm.projects = [
+    {
+      id: 1,
+      name: 'My Project',
+      hours: 2
+    },
+    {
+      id: 2,
+      name: 'My Project 2',
+      hours: 4
+    }
+  ];
+  vm.addProject = function() {
+    console.log('add project:', vm.newProject);
+    
+  };
+
+  vm.deleteProject = function(id) {
+    console.log('delete project:', id);
+    
+  };
 }]);
