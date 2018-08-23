70c4c1808b738f22bb1a56bf500d2cc1832441a1
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-23 10:22:08 -0500

    Finished GET projects query, reorganized SQL setup.

--- database_setup.sql
+++ database_setup.sql
@@ -1,32 +1,42 @@
--- POSTGRESQL DEV DATABASE SETUP
+-- reset projects table
+DROP TABLE entries;
+DROP TABLE projects;
 CREATE TABLE projects(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100)
 );
+INSERT INTO projects ("name") VALUES ('Project 1'), ('Project 2');
+SELECT * FROM projects;
 
+
+-- reset entries table
+DROP TABLE entries;
 CREATE TABLE entries (
 	id SERIAL PRIMARY KEY,
 	text VARCHAR(255),
 	project_id integer REFERENCES projects,
 	entry_date date,
-	start_time VARCHAR(10),
-	end_time VARCHAR(10)
+	start_time time,
+	end_time time
 );
-
-DROP TABLE entries;
-
-INSERT INTO projects ("name") VALUES ('Project 1'), ('Project 2');
-
-SELECT * FROM projects;
-
 INSERT INTO entries ("text", "project_id", "entry_date", "start_time", "end_time") VALUES 
-('entry 1', '1', '8/8/18', '9:00am', '5:00pm'),
-('entry 2', '2', '9/9/19', '9:00am', '5:00pm');
+('entry 1', '1', '8/8/18', '9:00 AM', '11:30 AM'),
+('entry 2', '1', '8/8/18', '9:00 AM', '11:00 AM'),
+('entry 3', '2', '9/9/19', '9:00 AM', '10:30 AM');
+SELECT * FROM entries;
 
--- This gets all entries, with "entry_date" formatted as 'MM/DD/YY', along with the project_name, and difference between start_time and end_time as hours
+
+-- get all entries with entry_date formated as 'MM/DD/YY', along with "project_name" and "hours"
 SELECT "entries".*,
 	to_char("entries"."entry_date", 'MM/DD/YY') AS "entry_date",
 	"projects"."name" as "project_name", 
 	DATE_PART('hour', "entries"."end_time"::time - "entries"."start_time"::time) + 
 	DATE_PART('MINUTE', "entries"."end_time"::time - "entries"."start_time"::time) / 60 AS "hours" 
 	FROM "entries" JOIN "projects" ON "entries"."project_id" = "projects"."id";
+
+-- get all projects, including the "total_hours" from all entries for that project
+SELECT "projects".*, 
+	SUM(DATE_PART('hour', "entries"."end_time"::time - "entries"."start_time"::time) + 
+		DATE_PART('MINUTE', "entries"."end_time"::time - "entries"."start_time"::time) / 60) 
+		AS "total_hours" 
+	FROM "projects" JOIN "entries" ON "projects"."id" = "entries"."project_id" GROUP BY "projects"."id" ORDER BY "projects"."id";
