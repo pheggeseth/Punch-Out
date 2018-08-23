986badf225c6643b2b4061c90e2e9c22cf67cc77
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-23 00:35:45 -0500

    Other touches to entries table. Redid main nav.

--- server/public/scripts/controllers/entries.controller.js
+++ server/public/scripts/controllers/entries.controller.js
@@ -10,15 +10,37 @@ app.controller('EntriesController', ['$http', function($http) {
     end_time: ''
   };
 
+  vm.entries = [
+    {
+      id: 1,
+      text: 'first entry',
+      project: 'project 1',
+      date: '8/8/18',
+      hours: 2
+    },
+    {
+      id: 2,
+      text: 'second entry',
+      project: 'project 2',
+      date: '9/9/19',
+      hours: 4
+    }
+  ];
+
   vm.projects = [
     {
       id: 1,
       name: 'My Project'
     }
   ];
-
+  vm.getEntries = function() {
+    
+  }
   vm.addEntry = function() {
     console.log('add new entry:', vm.newEntry);
     
+  };
+  vm.deleteEntry = function(id) {
+    console.log('delete entry:', id);
   }
 }]);
